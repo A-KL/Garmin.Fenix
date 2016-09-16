c6e13838d87872122a441d2dcb5949473fe4a69f
Anatolii Klots <Anatolii.Klots@albumprinter.com>
2016-09-16 17:20:59 +0200

    Initial

---
+++ src/Garmin.Fenix.Core/V1/GarminDevice.cs
@@ -0,0 +1,14 @@
+﻿namespace Garmin.Fenix.Core.V1
+{
+    public class GarminDevice
+    {
+        private string rootPath;
+
+        public GarminDevice(string path)
+        {
+            this.rootPath = path;
+        }
+
+
+    }
+}
